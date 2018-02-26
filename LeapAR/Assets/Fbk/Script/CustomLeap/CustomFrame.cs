1b97f86a34cbc60565a4bfe8fe420853b76262c1
Unknown <caterina.battisti@studenti.unitn.it>
2018-02-26 12:33:56 +0100

    add base project
    hands are showing but with a delay

---
+++ LeapAR/Assets/Fbk/Script/CustomLeap/CustomFrame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomLeap
+{
+    [Serializable]
+    public class CustomFrame
+{
+        public float currentFrameRate { get; set; }
+        public List<object> devices { get; set; }
+        public List<object> gestures { get; set; }
+        public List<Hand> hands { get; set; }
+        public long id { get; set; }
+        public InteractionBox interactionBox { get; set; }
+        public List<Pointable> pointables { get; set; }
+        public List<List<double>> r { get; set; }
+        public double s { get; set; }
+        public List<double> t { get; set; }
+        public long timestamp { get; set; }
+    }
+}
---
+++ LeapAR/Assets/Fbk/Script/FrameProviderNew.cs
@@ -0,0 +1,76 @@
+﻿// ===============================
+// AUTHOR     : Mirko Pani
+// CREATE DATE     : 24/10/17
+// PURPOSE     : Provider dei Frame usato da LeapServiceProvider per ottenere frame
+// SPECIAL NOTES: SDK Orion v3.2
+// ===============================
+// Change History:
+//
+//==================================
+
+
+
+
+using Leap;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameProviderNew : MonoBehaviour
+{
+    WebsocketConnection customFrameProvider;
+    public static List<Frame> frameBuffer;
+    public int maxBufferSize = 100;
+    public bool sorting = false;
+
+    
+    public Frame LatestFrame
+    {
+        get
+        {
+            return frameBuffer.Count > 2 ? frameBuffer[frameBuffer.Count - 1] : new Frame(); 
+        }
+        set { LatestFrame = value; }
+    }
+
+
+    void Start()
+    {
+        frameBuffer = new System.Collections.Generic.List<Frame>();
+        customFrameProvider= GetComponent<WebsocketConnection>();
+    }
+
+   
+    void Update()
+    {
+        while (customFrameProvider.buffer.Count > 0)
+        {
+            Frame newFrame = customFrameProvider.buffer[0];
+            customFrameProvider.buffer.RemoveAt(0);
+
+            if (newFrame != null)
+            {
+                //Ordina i frame secondo l'id
+                if (sorting && frameBuffer.Count > 1)
+                {
+                    int i = frameBuffer.Count - 1;
+                    while (frameBuffer[i].Id > newFrame.Id && i > 0)
+                    {
+                        i--;
+                    }
+                    frameBuffer.Insert(++i, newFrame);
+                }
+                else
+                {
+                    frameBuffer.Add(newFrame);
+                }
+
+
+                while (frameBuffer.Count > maxBufferSize)
+                {
+                    frameBuffer.RemoveAt(0);
+                }
+            }
+        }
+    }
+
+}
